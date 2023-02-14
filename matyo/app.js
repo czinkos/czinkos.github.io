b41851708381bfdcc22359d192109d429ce7a748
Zsolt Czinkos <czinkos@gmail.com>
2023-02-14 20:29:10 +0100

    x

--- matyo/app.js
+++ matyo/app.js
@@ -32268,8 +32268,8 @@ ${e}`);
       const fx = (x3) => x3 * scale - this.halfWidth;
       const fy = (y3) => y3 * scale - this.halfHeight;
       this.app.view.addEventListener(eventName, (e) => {
+        console.log(eventName);
         const n = this.simulation.find(fx(e.x), fy(e.y), 30 * scale);
-        console.log(n);
         return fn(n, e.x, e.y);
       });
     }
@@ -32545,15 +32545,8 @@ ${e}`);
           postFn: () => {
             select_default2("div.story").classed("hover", true);
             const sim = this.simulation;
-            sim.on("mousemove", (n) => {
+            sim.on("touchstart", (n) => {
               n === void 0 ? sim.drawLabels([]) : sim.drawLabels([n], "withBg");
-            });
-            sim.on("click", (n) => {
-              if (n === void 0) {
-                sim.drawLabels([]);
-              } else {
-                sim.drawLabels([n], "withBg");
-              }
             });
           }
         })
